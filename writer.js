862b9d316797d43d2f42b96d6f7c7a0e9765dce4
Yash <yashp241195@gmail.com>
2017-10-26 04:25:49 +0530

    Update writer.js

--- writer.js
+++ writer.js
@@ -40,3 +40,37 @@ function cout(string,end = null){
 function newLine(string){
 	cout("","\n*"+string.toString());
 }
+
+function print1DArray(arr,fromIndex,toIndex){
+	if (arr != null){
+		maxIndex = arr.length - 1;
+		
+		
+		if(fromIndex <= toIndex){
+			
+			if(fromIndex >= 0 && toIndex <= maxIndex){
+				
+				table = "<table class = \"table_class\">";
+				IndexData = "<tr class = \"table_row\"><th style=\"color:green;\">Index </th>";
+				tableData = "<tr class = \"table_row\"><th style=\"color:green;\">Data  </th>";
+
+				for(i = fromIndex; i <= toIndex; i++){
+
+					IndexData += "<th>"+i+"</th>"; 
+					tableData += "<td>"+arr[i]+"</td>";
+				}
+				IndexData += "</tr>";
+				tableData += "</tr>";
+				table += IndexData + tableData +"</table>";
+
+				newLine(2);
+				cout(table);
+				newLine(2);
+			}
+		}
+		else {
+			newLine(2);
+			cout("Can't View the Array","\n*2");
+		}
+	}
+}

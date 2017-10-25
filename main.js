ba22e67c8f5cf706ddd9986da982e86f7630a2cc
Yash <yashp241195@gmail.com>
2017-10-26 02:11:56 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+// main JavaScript
+
+function main(){
+
+	var LL = new LinkedList();
+	cout("Console Output","\n*2");
+	LL.insert(1);
+	LL.insert(2);
+	LL.insert(3);
+	LL.insert(4);
+	LL.printList();
+}
+		
+main();

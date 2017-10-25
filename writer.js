8eed02d7501cb512399845a5b70bf2d456d18ad8
Yash <yashp241195@gmail.com>
2017-10-26 02:09:15 +0530

    Update writer.js

--- writer.js
+++ writer.js
@@ -1,36 +1,38 @@
 // Use to write on the HTML page as Console
 
 function cout(string,end = null){
-			delimiter = '';
-			counter = 1
-			MaxItr = parseInt(end[end.length-1])
-			console.log(MaxItr==NaN)
-			if(MaxItr != NaN){
-				
-				if(MaxItr < 7){
-					counter = MaxItr;
-				}
-				end = end.substr(0,end.length-2)
-				
+	
+	delimiter = '';
+	counter = 1
+	
+	if(end != null && end.length > 1){
+		
+		MaxItr = parseInt(end[end.length-1])
+		// if last element is a Number 
+		if(MaxItr != NaN){
+			// maximum delimiter
+			if(MaxItr < 7){
+				counter = MaxItr;
 			}
-
-				for (var i = 0;i< counter; i++){
-
-					switch(end){
-					
-					case "\n": delimiter += '<br>';
-								break;
-					case "\t": 
-								delimiter += '&nbsp;&nbsp;&nbsp;';
-								
-								break;
-					case ",": delimiter +=', ';
-								break;
-								
-					default:  delimiter += ' ';
-	        		}
-	    		}
-			
-           	var out = string + delimiter;
-			document.write(out);
+			end = end.substr(0,end.length-2)
 		}
+	}
+	
+	for (var i = 0; i < counter; i++){
+
+			switch(end){
+			
+			case "\n": delimiter += '<br>';
+						break;
+			case "\t": delimiter += '&nbsp;&nbsp;&nbsp;';
+						break;
+			case ",": delimiter +=', ';
+						break;
+						
+			default:  delimiter += ' ';
+    		}
+		}
+	
+   	var out = string + delimiter;
+	document.write(out);
+}

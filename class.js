1cdf3128e54a900516596f815855c2e26e5891ae
Yash <yashp241195@gmail.com>
2017-10-26 02:11:13 +0530

    Create class.js

---
+++ class.js
@@ -0,0 +1,39 @@
+// main JavaScript
+
+// Class defination with constructor
+
+function Node(data,next = null){
+	this.data = data;
+	this.next = next;
+}
+
+function LinkedList(start = null){
+	this.start = start;
+}
+
+	// defining the methods of Linked List
+
+	LinkedList.prototype.insert = function method(data) {
+		if(this.start == null){
+			this.start = new Node(data);
+
+		}else{
+			var i = this.start;
+			while(i.next != null){
+				i=i.next;
+			}
+			cout("Inserting "+i.data,"\n")
+			i.next = new Node(data);
+		}
+	};
+
+	LinkedList.prototype.printList = function method(){
+		
+		var i = this.start;
+		while(i != null){
+			
+			cout(i.data,"\t*3");
+			i = i.next;
+		}
+		
+	}

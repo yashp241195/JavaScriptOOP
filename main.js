44cbb561e68314f999a8c637a2040c64fac3b81c
Yash <yashp241195@gmail.com>
2017-10-26 10:42:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 // main JavaScript
 
-function main(){
+function main(is){
+
+
 
 	var LL = new LinkedList();
 	cout("Console Output","\n*2");
-	LL.insert(1);
-	LL.insert(2);
-	LL.insert(3);
-	LL.insert(4);
+	LL.insert(cin(is));
+	LL.insert(cin(is));
+	LL.insert(cin(is));
+	LL.insert(cin(is));
 	LL.printList();
-
-	arr = [11,12,11,11];
+	
+	arr = ["YY",12,11,11];
 	arr2 = [[9,8,7,6],[2,5,1,0]];
 
 	print1DArray(arr,0,3)
-
-	
 	print2DArray(arr2,0,1,0,3);
 
 	
+			
 }
-		
-main();
+function cin(arr){
+	temp = arr.shift();
+	return temp
+}
+
+
+

99d4dc8d8a0ed4ca4064273b22087f25abb342d4
Yash <yashp241195@gmail.com>
2017-10-29 10:13:01 +0530

    Update string.js

--- packages/string.js
+++ packages/string.js
@@ -1,17 +1,31 @@
-function getASCII(char){
+function char2int(char){
 	
-	if(char.length == 1){
+		if(char.length == 1){
 		
 		if(typeof(char) == "string"){
 			return char.charCodeAt(0);
-		} 
-	}
-	else{
-		cout("ASCII value of first character is returned ","\n");		
-		return char.charCodeAt(0);
-	}
+			} 
+		}
+		else{
+			cout("ASCII value of first character is returned ","\n");		
+			return char.charCodeAt(0);
+		}
+	
+	
 }
 
 function str2Array(string){
 	return string.split("");
+}
+
+function int2char(code){
+	val = parseInt(code);
+	
+		if(val >= 0 && val <= 255 ){
+			return String.fromCharCode(code);
+		}
+	
+		cout(code+" does not represent any valid ascii","\n");	
+		return -1;
+
 }
